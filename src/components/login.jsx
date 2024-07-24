fbb71d12aa9413d37e7149263ec86c9f81abd5c7
05-ram <karthik98@gmail.com>
2024-07-24 11:20:49 +0530

    clean up

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,34 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-const Login = () => {
-    const initialValues = { name: '', email: '', password: '' };
-    const [formvalue, setFormvalue] = useState(initialValues);
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        // setFormvalue({ ...formvalue, [name]: value })
-        console.log(name, value)
-    }
+const login = () => {
     return (
-        <div>
-            <form className='login-form'>
-                <div className="container">
-                    <div className='mb-3'>
-                        <label>Username </label><br />
-                        <input type="text" placeholder="Enter Username" name="username" required onChange={handleChange} value={initialValues.name} />
-                    </div>
-                    <div className='mb-3'>
-                        <label>Email </label><br />
-                        <input type="email" placeholder="Enter email" name="email" required onChange={handleChange} value={initialValues.email} />
-                    </div>
-                    <div className='mb-3'>
-                        <label>Password </label><br />
-                        <input type="password" placeholder="Enter Password" name="password" required onChange={handleChange} value={initialValues.password} />
-                    </div>
-                    <button type="submit" className='btn'>Submit</button>
-                </div>
-            </form>
-        </div>
+        <div>login</div>
     )
 }
 
-export default Login
+export default login
